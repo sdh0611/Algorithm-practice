31440e9cf7ef99786eea90e8b98a2606e1d480ce
sdh0611 <tlaeogus0611@naver.com>
2019-10-21 18:00:15 +0900

    10.21 : Truck.

--- StackQueue/StackQueue4/StackQueue4/main.cpp
+++ StackQueue/StackQueue4/StackQueue4/main.cpp
@@ -5,57 +5,23 @@
 
 using namespace std;
 
-int solution(int BridgeLength, int Weight, vector<int> TruckWeights) 
+int Solution(int BridgeLength, int Weight, vector<int> TruckWeights) 
 {
-	typedef struct Truck
-	{
-		int Weight = 0;
-		int Distance = 0;
-
-		Truck(int NewWeight, int NewDistance)
-			:Weight(NewWeight), Distance(NewDistance)
-		{
-		}
-
-	}Truck;
-
 	int Seconds = 0;
 
-	list<int> WaitTrucks;
-	WaitTrucks.assign(TruckWeights.begin(), TruckWeights.end());
-
-	list<int> TruckOnBridge;
-
-	int BridgeWeight = 0;
-
-	do
+	list<int> TruckOnBridge(TruckWeights.begin(), TruckWeights.end());
+	
+	int TotalWeight = 0;
+	while(true)
 	{
-		if (BridgeWeight + WaitTrucks.front() < Weight)
+		if (TotalWeight < Weight)
 		{
-			BridgeWeight += WaitTrucks.front();
-			TruckOnBridge.push_back(0);
-			//WaitTrucks.pop_front();
+		
 		}
 
-		for (auto& Truck : TruckOnBridge)
-		{
-			++Truck;
-		}
-
-		if (TruckOnBridge.front()> BridgeLength)
-		{
-			BridgeWeight -= TruckOnBridge.front().Weight;
-			WaitTrucks.pop_front();
-			if (BridgeWeight < 0)
-			{
-				BridgeWeight = 0;
-			}
-			
-			TruckOnBridge.pop_front();
-		}
 
 		++Seconds;
-	} while (!(TruckOnBridge.empty() && WaitTrucks.empty()));
+	}
 
 
 	return Seconds;
@@ -84,7 +50,7 @@ int main()
 		Weights.push_back(Input);
 	}
 
-	cout << "Seconds : " << solution(Length, Weight, Weights) << "\n";
+	cout << "Seconds : " << Solution(Length, Weight, Weights) << "\n";
 
 	return 0;
 }
